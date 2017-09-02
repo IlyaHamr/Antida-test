3828af3d4f1c548c46161f710660b2e329f8291d
Ilya Hamraev <hamraev9517@gmail.com>
2017-09-02 11:42:49 +0500

    passing key

--- src/App.js
+++ src/App.js
@@ -9,16 +9,14 @@ class App extends Component {
             albums: []
         };
         this.handlerClick = this.handlerClick.bind(this);
-
-    }
+    };
 
 
     search() {
-        //console.log('this.state', this.state);
-        var API_KEY = '7ff79219b52c177d131ee7e19f553323'
-        var text = document.getElementsByTagName('input')[0];
-        var value = text.value;
-        var FETCH_URL = 'http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=' + value + '&api_key=' + API_KEY + '&format=json';
+        const API_KEY = '7ff79219b52c177d131ee7e19f553323';
+        const text = document.getElementsByTagName('input')[0];
+        const value = text.value;
+        const FETCH_URL = 'http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=' + value + '&api_key=' + API_KEY + '&format=json';
 
         fetch(FETCH_URL)
             .then(response => response.json())
@@ -28,37 +26,56 @@ class App extends Component {
                 //console.log(items);
             });
 
+
+
+
     };
 
     handlerClick(event) {
         event.stopPropagation();
         //console.log(event.currentTarget.innerHTML);
         var value = event.currentTarget.innerHTML;
-        var RegExp = /\s/g;
-        value = value.replace(RegExp, '');
         //console.log(value)
-        var API_KEY = '7ff79219b52c177d131ee7e19f553323'
+        this.setState({query : value})
+        const RegExp = /\s/g;
+        value = value.replace(RegExp, '+');
+        //console.log(value)
+        var imageAlbums = [];
+        const API_KEY = '7ff79219b52c177d131ee7e19f553323'
         const FETCH_URL = "http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=" + value + "&api_key=" + API_KEY + "&format=json";
 
 
         fetch(FETCH_URL)
             .then(response => response.json())
             .then( json => {
-                if( typeof json.topalbums['album'] == 'undefined') {
-                    console.log(undefined)
-                } else {
-                    this.setState({albums: json.topalbums['album']})
-                    console.log(json.topalbums['album'])
+                var all = json.topalbums.album;
+                for (var i = 0; i< all.length; i++) {
+                    //console.log(all[i].image[2]['#text'])
+                    imageAlbums.push(all[i].image[2]['#text'])
                 }
-                //this.setState({albums: json.topalbums.album})
-
-                //var alb = json.album;
-                //console.log(json.topalbums)
-                //this.state({ albums : json.topalbums.album });
+                this.setState({ albums: imageAlbums});
             });
 
-    };
+        var blockAlbums = document.getElementById('blockAlbums');
+        blockAlbums.style.display="block";
+        var blockList = document.getElementById('blockList');
+        blockList.style.display='none';
+        var inputGroup = document.getElementById('inputGroup');
+        inputGroup.style.display='none';
 
+        const btn = document.getElementById('btnBack');
+        btn.addEventListener('click', function () {
+            if (blockList.style.display='none') {
+                blockAlbums.style.display='block';
+                inputGroup.style.display='none'
+            }
+            if (blockAlbums.style.display='none') {
+                blockList.style.display='block';
+                inputGroup.style.display='block'
+            }
+        })
+
+    };
 
 
 
@@ -69,7 +86,7 @@ class App extends Component {
         <div className="container">
             <hr />
             <div className="col-lg-6">
-                <div className="input-group">
+                <div id="inputGroup" className="input-group">
                     <input className="inpName" type="text"
                         onChange={event => { this.setState({ query: event.target.value }) }}
                     className="form-control" placeholder="Введите исполнителя" />
@@ -81,19 +98,23 @@ class App extends Component {
                 </div>
             </div>
             <hr />
-            <div>
+            <div id="blockList">
                 <ul>
                     {this.state.list.map(e => (
-                        <li key={e.name}>
+                        <p key={e.name}>
                             <a className='link' onClick={this.handlerClick} href="#">{e.name}</a>
-                        </li>
+                        </p>
                     ))}
                 </ul>
             </div>
-            <div>
-                <ul>
-
-                </ul>
+            <div id="blockAlbums">
+                <div><p className="Caption">Альбомы {this.state.query}</p></div>
+                <div id='clear'><button className="btn" id='btnBack'>Назад</button></div>
+                <div className="block">
+                    {this.state.albums.map((e, key) => (
+                        <img className="imgAlbum" key={key} src={e}></img>
+                    ))}
+                </div>
             </div>
         </div>
     );
