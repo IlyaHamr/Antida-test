e78fe48796df7b258fbf2f49804afd86c9107ff3
Ilya Hamraev <hamraev9517@gmail.com>
2017-09-03 19:42:58 +0500

    ready task

--- src/App.js
+++ src/App.js
@@ -1,124 +1,98 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 class App extends Component {
 
-    constructor(props) {
-        super(props);
+    constructor (props) {
+        super(props)
         this.state = {
             list: [],
             query: '',
-            albums: []
-        };
-        this.handlerClick = this.handlerClick.bind(this);
+            albums: [],
+            isAlbumOpen: false,
+            isArtistOpen: false
+        }
+        this.handlerClick = this.handlerClick.bind(this)
+        this.handleBack = this.handleBack.bind(this)
+        this.renderAlbums = this.renderAlbums.bind(this)
     };
 
-
-    search() {
-        const API_KEY = '7ff79219b52c177d131ee7e19f553323';
-        const text = document.getElementsByTagName('input')[0];
-        const value = text.value;
-        const FETCH_URL = 'http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=' + value + '&api_key=' + API_KEY + '&format=json';
+    search () {
+        const API_KEY = '7ff79219b52c177d131ee7e19f553323'
+        const text = document.getElementsByTagName('input')[0]
+        const value = text.value
+        const FETCH_URL = 'http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=' + value + '&api_key=' + API_KEY + '&format=json'
 
         fetch(FETCH_URL)
             .then(response => response.json())
             .then(json => {
-                this.setState({ list: json.results.artistmatches.artist });
-                //var items = json.results.artistmatches.artist;
-                //console.log(items);
-            });
-
-
-
-
+            this.setState({list: json.results.artistmatches.artist, isArtistOpen: true})
+            })
     };
 
-    handlerClick(event) {
-        event.stopPropagation();
-        //console.log(event.currentTarget.innerHTML);
-        var value = event.currentTarget.innerHTML;
-        //console.log(value)
-        this.setState({query : value})
-        const RegExp = /\s/g;
-        value = value.replace(RegExp, '+');
-        //console.log(value)
-        var imageAlbums = [];
+    handlerClick (value, event) {
+        const RegExp = /\s/g
         const API_KEY = '7ff79219b52c177d131ee7e19f553323'
-        const FETCH_URL = "http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=" + value + "&api_key=" + API_KEY + "&format=json";
-
+        const FETCH_URL = 'http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=' + value.replace(RegExp, '+') + '&api_key=' + API_KEY + '&format=json'
 
         fetch(FETCH_URL)
             .then(response => response.json())
-            .then( json => {
-                var all = json.topalbums.album;
-                for (var i = 0; i< all.length; i++) {
-                    //console.log(all[i].image[2]['#text'])
-                    imageAlbums.push(all[i].image[2]['#text'])
-                }
-                this.setState({ albums: imageAlbums});
-            });
-
-        var blockAlbums = document.getElementById('blockAlbums');
-        blockAlbums.style.display="block";
-        var blockList = document.getElementById('blockList');
-        blockList.style.display='none';
-        var inputGroup = document.getElementById('inputGroup');
-        inputGroup.style.display='none';
-
-        const btn = document.getElementById('btnBack');
-        btn.addEventListener('click', function () {
-            if (blockList.style.display='none') {
-                blockAlbums.style.display='block';
-                inputGroup.style.display='none'
+            .then(json => {
+            this.setState({albums: json.topalbums.album.map(e => e.image[2]['#text']), isAlbumOpen: true, isArtistOpen: false, query: value})
             }
-            if (blockAlbums.style.display='none') {
-                blockList.style.display='block';
-                inputGroup.style.display='block'
-            }
-        })
-
+        )
     };
 
+    handleBack (e) {
+        e.preventDefault()
+        this.setState({ isArtistOpen: true, isAlbumOpen: false })
+    };
 
+    renderAlbums () {
+        if (this.state.albums.length) {
+            return this.state.albums.map((e, key) => <img className='imgAlbum' key={key} alt='artist img' src={e} />)
+        }  else {
+            return <h2>No albums</h2>
+        }
+    };
 
-    render() {
-    // return JSX
+    render () {
+        // return JSX
 
-    return (
-        <div className="container">
-            <hr />
-            <div className="col-lg-6">
-                <div id="inputGroup" className="input-group">
-                    <input className="inpName" type="text"
-                        onChange={event => { this.setState({ query: event.target.value }) }}
-                    className="form-control" placeholder="Введите исполнителя" />
-                    <span className="input-group-btn">
-                    <button
-                        onClick={()=> this.search()}
-                        className="btn btn-default" type="button">Искать</button>
-                    </span>
+        return (
+            <div className='container'>
+                <hr />
+                <div className='col-lg-6'>
+                    <div id='inputGroup' className='input-group'>
+                        <input type='text' onChange={e => { this.setState({query: e.target.value}) }}
+                            className='form-control inpName' placeholder='Введите исполнителя' />
+                        <span className='input-group-btn'>
+                            <button
+                                onClick={() => this.search()}
+                                className='btn btn-default' type='button'>Искать</button>
+                        </span>
+                    </div>
                 </div>
-            </div>
-            <hr />
-            <div id="blockList">
-                <ul>
-                    {this.state.list.map(e => (
-                        <p key={e.name}>
-                            <a className='link' onClick={this.handlerClick} href="#">{e.name}</a>
-                        </p>
-                    ))}
-                </ul>
-            </div>
-            <div id="blockAlbums">
-                <div><p className="Caption">Альбомы {this.state.query}</p></div>
-                <div id='clear'><button className="btn" id='btnBack'>Назад</button></div>
-                <div className="block">
-                    {this.state.albums.map((e, key) => (
-                        <img className="imgAlbum" key={key} src={e}></img>
-                    ))}
+                <hr />
+                <div className={this.state.isArtistOpen ? '' : 'hidden'}>
+                    <ul>
+                        {this.state.list.map(e => (
+                            <p key={e.name}>
+                                <a className='link' onClick={this.handlerClick.bind(this, e.name)}>{e.name}</a>
+                            </p>
+                        ))}
+                    </ul>
+                </div>
+                <div className={this.state.isAlbumOpen ? '' : 'hidden'}>
+                    <div><p className='Caption'>Альбомы {this.state.query}</p></div>
+                    <div id='clear'>
+                        <button className='btn' onClick={this.handleBack}>Назад</button>
+                    </div>
+                <div className='block'>
+                    { this.renderAlbums() }
                 </div>
             </div>
         </div>
-    );
-  }
+        )
+    }
 }
 
-export default App;
+export default App
